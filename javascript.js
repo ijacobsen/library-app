38671777e2a9b4e96ee065a2f1f7060805cc3ec8
ijacobsen <ian.jacobsen.92@gmail.com>
2022-06-28 15:19:59 -0400

    working on displaying library

--- javascript.js
+++ javascript.js
@@ -1,26 +1,3 @@
-let myLibrary = []
-
-function Book(author, title, pages, been_read){
-    this.author = author;
-    this.title = title;
-    this.pages = pages;
-    this.been_read = been_read;
-
-}
-
-function addBookToLibrary(){
-
-    let author = data.get('author');
-    let title = data.get('title');
-    let pages = data.get('pages');
-    let been_read = data.get('read');
-
-    let book = new Book(author, title, pages, been_read);
-
-    myLibrary.push(book);
-
-}
-
 /*
 
 - write a function that loops through the array and
@@ -49,25 +26,101 @@ the library
 
 */
 
+/*
+=======================================
+globals
+=======================================
+*/
 
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        buttonPress(button.id)
-    })
-});
-
-function buttonPress(userSelection){
-    console.log('user requests adding a book');
-    openForm()
-    addBookToLibrary()
-}
-
-function openForm() {
-    document.getElementById('popupForm').style.display = 'block';
+let myLibrary = []
+function Book(author, title, pages, been_read){
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.been_read = been_read;
 }
 
 function closeForm() {
-    document.getElementById('popupForm').style.display = 'none';
+    document.getElementById('bookData').style.display = 'none';
 }
 
+function displayBooks() {
+    /*
+    // delete current table
+    let table = document.getElementById('library');
+    librarySection = table.parentNode;
+    table.remove();
+    */
+
+    // create new table
+    let table = document.getElementById('library');
+    for (var idx = 0; idx < myLibrary.length; idx++){
+        let newRow = table.insertRow(-1);
+        let newCell = newRow.insertCell(0);
+        let newText = document.createTextNode('New row');
+        newCell.appendChild(newText);
+    }
+    //librarySection.appendChild(table);
+}
+
+/*
+=======================================
+bring up popup form
+=======================================
+*/
+var addNewBook = document.getElementById('addBook');
+addNewBook.addEventListener('click', newBookPopUp)
+function newBookPopUp () {
+    openForm();
+    console.log('user requests adding book')
+}
+function openForm() {
+    document.getElementById('bookData').style.display = 'block';
+}
+
+/*
+=======================================
+add book to library
+=======================================
+*/
+var submitBook = document.getElementById('submitBook');
+submitBook.addEventListener('click', addBookToLibrary)
+function addBookToLibrary(){
+    var form = document.getElementById('bookForm');
+    const data = new FormData(form);
+    let author = data.get('author');
+    let title = data.get('title');
+    let pages = data.get('pages');
+    let been_read = data.get('read');
+    let book = new Book(author, title, pages, been_read);
+    console.log(book);
+    myLibrary.push(book);
+    console.log(myLibrary);
+    closeForm();
+    displayBooks();
+}
+
+/*
+=======================================
+remove book from library
+=======================================
+*/
+
+function deleteBook(o) {
+    var p = o.parentNode.parentNode;
+    p.parentNode.removeChild(p);
+}
+
+/*
+    <table>
+       <tr>
+           <td><input type="button" value="Delete Row" onclick="SomeDeleteRowFunction(this)"></td>
+       </tr>
+       <tr>
+           <td><input type="button" value="Delete Row" onclick="SomeDeleteRowFunction(this)"></td>
+       </tr>
+       <tr>
+           <td><input type="button" value="Delete Row" onclick="SomeDeleteRowFunction(this)"></td>
+       </tr>
+    </table>
+*/
